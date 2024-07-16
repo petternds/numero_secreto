2300cb195e406ac87af1e1c05b8f7d67b991266e
Peter <petternds@gmail.com>
2024-07-16 19:16:02 -0300

    implementando o reconhecimento de voz

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,3 +1,5 @@
+const elementoChute = document.getElementById('chute');
+
 window.SpeechRecognition = window.SpeechRecognition ||
     webkitSpeechRecognition;
 
@@ -5,3 +7,18 @@ const recognition = new SpeechRecognition();
 
 recognition.lang = 'pt-Br';
 recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+    chute = e.results[0][0].transcript;
+    exibeChuteNaTela(chute);
+
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+    <div>Você disse:</div>
+    <span class="box"> ${chute}</span>
+`
+}
